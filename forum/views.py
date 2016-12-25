1d04a98a9bc8fdeda72f1f98a27e87c4cc26d36f
Mehdi BAHA <elmehdi.baha@gmail.com>
2016-12-25 15:54:54 +0000

    Remove unnecessary files

--- forum/views.py
+++ forum/views.py
@@ -6,7 +6,7 @@ import requests
 from storage import check_credentials, get_company
 from mailing import send_mail
 
-data = {}
+data = {'name': 'SNCF'}
 
 # start of app
 @app.route('/', methods=["GET"])
@@ -14,12 +14,12 @@ def index():
     return render_template('index.html')
 
 @app.route('/admin')
-def admin():
-    return render_template('admin.html', company=data)
-
-@app.route('/admin/<page_name>')
-def get_page(page_name):
-    return render_template('{}.html'.format(page_name), company=data)
+@app.route('/admin/<page>')
+def admin(page):
+    if page:
+        return render_template('admin.html', company=data)
+    else:
+        return render_template('{}.html'.format(name), company=data)
 
 @app.route('/connexion', methods=["GET", "POST"])
 def login():
