471a082b13f44e620f12e7ce3d7afbacc4d176e8
Mehdi BAHA <elmehdi.baha@gmail.com>
2016-12-26 06:55:57 +0000

    Small fix for julia

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 web: gunicorn forum:app
-init: mongoimport -v --db heroku_lx65hjrq --collection companies --file sample.json --upsert --upsertFields=idf
 db: sudo mongod
 dev: python ./runserver.py
--- forum/storage.py
+++ forum/storage.py
@@ -2,15 +2,20 @@ from pymongo import MongoClient
 import os
 import json
 
+fn = os.path.join(os.path.dirname(__file__), '..', 'sample.json')
+with open(fn) as data_file:
+	data = json.load(data_file)
+
 if os.environ.get('env') == 'dev':
 	def get_company(company_id):
-		fn = os.path.join(os.path.dirname(__file__), '..', 'sample.json')
-		with open(fn) as data_file:
-			data = json.load(data_file)
-			return data
+		return data	
 else:
 	client = MongoClient(host=os.environ.get('MONGODB_URI'))
 	db = client.heroku_lx65hjrq
+	try:
+		db.companies.insert_one(data)
+	except:
+		print("Oups. Already created.")
 
 	def get_company(company_id):
 		return db.companies.find_one({"id": company_id})
