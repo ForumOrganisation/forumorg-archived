568df20c1f33540a439f0b86b34b2d425dcc8c64
Mehdi BAHA <elmehdi.baha@gmail.com>
2016-12-27 17:50:53 +0000

    Add display for transport

--- forum/__init__.py
+++ forum/__init__.py
@@ -1,5 +1,9 @@
+# coding: utf-8
+
 from flask import Flask
 from flask_login import LoginManager
+import datetime
+import locale
 
 app = Flask(__name__)
 app.secret_key = 'supa-secret'
@@ -8,5 +12,27 @@ app.secret_key = 'supa-secret'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+@app.template_filter('format_dt')
+def format_dt(dt):
+	locale.setlocale(locale.LC_ALL, "fr_FR")
+	dt = datetime.datetime.strptime(dt, format("%d/%m/%Y %H:%M"))
+	dt = dt.strftime("%a %d %b %H:%M")
+	return dt
+
+@app.template_filter('to_human')
+def to_human(num):
+	opts = {
+		"1": "un",
+		"2": "deux",
+		"3": "trois",
+		"4": "quatre",
+		"5": "cinq",
+		"6": "six",
+		"7": "sept",
+		"8": "huit",
+		"9": "neuf",
+	}
+	return opts[num]
 
 import views
