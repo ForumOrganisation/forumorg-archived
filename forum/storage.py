187b76f21787d66c71aaee70559a16f62d9429de
Mehdi BAHA <elmehdi.baha@gmail.com>
2016-12-29 15:17:50 +0000

    Add support for config vars credentials

--- forum/storage.py
+++ forum/storage.py
@@ -2,17 +2,16 @@ from pymongo import MongoClient
 import os
 import json
 
-fn = os.path.join(os.path.dirname(__file__), '..', 'companies.json')
-with open(fn) as file:
-	data = json.load(file)
 
 client = MongoClient(host=os.environ.get('MONGODB_URI'))
 db = client.heroku_lx65hjrq
-for c in data['companies']:
-	db.companies.replace_one({'id': c['id']}, c, upsert=True)
+# Creating admin account
+admin_doc = {'id': os.environ.get('ADMIN_ID'), 'name': 'Administrateur', 'password': os.environ.get('ADMIN_PASSWORD')}
+db.companies.replace_one({'id': admin_doc['id']}, admin_doc, upsert=True)
 
 def get_company(company_id):
 	company = db.companies.find_one({'id': company_id})
+	# dropping unserializable field
 	if company:
 		company.pop('_id', None)
 	return company
