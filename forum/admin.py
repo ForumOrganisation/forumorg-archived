53cf0ae2bf5d0fc7859761bd1d64b49f2bff2f19
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-01-04 00:01:29 +0100

    Add equipement to admin

--- forum/admin.py
+++ forum/admin.py
@@ -2,15 +2,27 @@ import json
 import os
 
 from flask import Response, flash, stream_with_context
+from flask_admin.babel import gettext
 from flask_admin.base import expose
 from flask_admin.contrib.pymongo import ModelView, filters
 from flask_admin.helpers import get_redirect_target
 from flask_login import current_user
 from mongoengine.errors import NotUniqueError
 from werkzeug import secure_filename
-from wtforms import fields, form
+from wtforms import StringField, fields, form, validators
 
 from export import generate_vals
+
+def sections_formatter(v,c,m,p):
+    if m['id'] != 'admin':
+        if p == 'nom complet':
+            return m['sections']['profile']['name']
+        if p in SECTIONS[6:]:
+            return m['sections']['equipement']['general'][p]
+        return m['sections'][p]['completed']
+SECTIONS = ['equipement', 'transport', 'restauration', 'programme', 'badges',\
+            'nom complet',\
+            'emplacement', 'duration', 'equiped', 'bandeau', 'size']
 
 def get_sections():
     fn = os.path.join(os.path.dirname(__file__), 'sections.json')
@@ -18,16 +30,25 @@ def get_sections():
         return json.load(file)
 
 class CompanyForm(form.Form):
-    id = fields.StringField('Identifiant')
-    password = fields.PasswordField('Mot de passe')
+    id = fields.StringField('Identifiant', validators=[validators.Required(), validators.Length(min=3, max=30)], render_kw={"placeholder": "Ex. LOREAL"})
+    password = fields.PasswordField('Mot de passe', validators=[validators.Required(), validators.Length(min=5, max=30)], render_kw={"placeholder": "Ex. 123456"})
+    name = fields.StringField('Nom complet', render_kw={"placeholder": "Ex. L'Oreal"})
+    emplacement = fields.StringField('Emplacement', render_kw={"placeholder": "Ex. F13"})
+    duration = fields.IntegerField('Jours de presence', render_kw={"placeholder": "Ex. 2"})
+    equiped = fields.BooleanField('Equipe?')
+    bandeau = fields.BooleanField('Bandeau?')
+    size = fields.IntegerField('Surface', render_kw={"placeholder": "Ex. 12"})
 
 class CompanyView(ModelView):
+    form = CompanyForm
+    column_list = ['id'] + SECTIONS[:5]
+    column_labels = dict(id='Identifiant')
+    column_formatters = dict((s,sections_formatter) for s in SECTIONS)
+    export_types = ['csv', 'transport', 'restauration', 'badges', 'equipement']
     can_export = True
     can_delete = True
-    column_list = ['id', 'password']
-    column_labels = dict(id='identifiant', password='mot de passe')
-    export_types = ['csv', 'transport', 'restauration', 'badges', 'equipement']
-    form = CompanyForm
+    column_details_list = SECTIONS[5:]
+    can_view_details = True
 
     def __init__(self, *args, **kwargs):
         super(CompanyView, self).__init__(*args, **kwargs)
@@ -39,7 +60,6 @@ class CompanyView(ModelView):
     def create_model(self, form):
         try:
             model = form.data
-            model['sections'] = self.sections
             self._on_model_change(form, model, True)
             self.coll.insert(model)
         except Exception as ex:
@@ -54,6 +74,34 @@ class CompanyView(ModelView):
             self.after_model_change(form, model, True)
 
         return model
+
+    def update_model(self, form, model):
+        try:
+            model.update(form.data)
+            self._on_model_change(form, model, False)
+            pk = self.get_pk_value(model)
+            self.coll.update({'_id': pk}, model)
+        except Exception as ex:
+            flash(gettext('Failed to update record. %(error)s', error=str(ex)),
+                  'error')
+            log.exception('Failed to update record.')
+            return False
+        else:
+            self.after_model_change(form, model, False)
+
+        return True
+
+    def _on_model_change(self, form, model, is_created):
+        if is_created:
+            model['sections'] = self.sections
+        if 'emplacement' in form.data:
+            model['sections']['equipement']['general']['emplacement'] = form.data.get('emplacement')
+            form.data.pop('emplacement', None)
+            model.pop('emplacement', None)
+        if 'name' in form.data:
+            model['sections']['profile']['name'] = form.data.get('name')
+            form.data.pop('name', None)
+            model.pop('name', None)
 
     def is_accessible(self):
         return current_user.get_id() == "admin" and current_user.is_authenticated
