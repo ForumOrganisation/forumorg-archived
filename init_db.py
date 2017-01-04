b49bc3dfcc1c98294665daf2cc6019ebcb5733e5
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-01-04 20:33:52 +0100

    Add init_db.py

---
+++ init_db.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+client = MongoClient(host=os.environ.get('MONGODB_URI'))
+db = client.get_default_database()
+
+# Creating index
+db.companies.create_index(keys='id', name='index_id', unique=True)
+
+# Creating admin account
+admin_data = dict(id=os.environ.get('ADMIN_ID'), password=os.environ.get('ADMIN_PASSWORD'))
+db.companies.replace_one({'id': admin_data['id']}, admin_data, upsert=True)
