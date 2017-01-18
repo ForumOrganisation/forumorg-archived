5e13528a3c2912b4238f8f531a8f2c2652292b0a
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-01-18 22:22:41 +0100

    Add init_db

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -8,8 +8,10 @@ def main():
         client = MongoClient(host=os.environ.get('MONGODB_URI'))
         db = client.get_default_database()
         # tasks
-        db.users.create_index(keys='id', name='index_id', unique=True)
-        create_admin(db)
+        # db.users.create_index(keys='id', name='index_id', unique=True)
+        # create_admin(db)
+        db.users.update_many({'$or': [{'events.joi.conference.registered': True}, {'events.joi.table_ronde.registered': True}]},
+        {'$set': {'events.joi.registered': True}})
     except Exception as e:
         print(e)
 
