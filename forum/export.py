a59192a4c2520ae942095b3586a87500422df20e
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-02-03 17:15:17 +0100

    Fix exports (YES)

--- forum/export.py
+++ forum/export.py
@@ -12,35 +12,33 @@ def generate_vals(writer, export_type, data):
     titles = ['id_entreprise']
     if export_type == 'equipement':
         titles += ['duration', 'equiped', 'banner', 'size', 'emplacement']
-        titles += data[0]['sections']['equipement']['furnitures'].keys()
+        titles += [u'chauffeuse', u'mange_debout', u'presentoir', u'ecran_32', u'ecran_42', u'poste_2', u'poste_3', u'poste_6', u'poste_9']
         yield writer.writerow(titles)
         for row in data:
             vals = []
             for t in titles[:1]:
                 vals.append(row.get('id', ''))
-            for t in titles[1:7]:
-                vals.append(row['sections']['equipement']['general'].get(t, 0))
-            for t in titles[7:]:
-                vals.append(row['sections']['equipement']['furnitures'][t].get('quantity'))
+            for t in titles[1:6]:
+                vals.append(row.get(t, 0))
+            for t in titles[6:]:
+                vals.append(row['sections']['furnitures'].get(t, 0))
             vals = [csv_encode(v) for v in vals]
             yield writer.writerow(vals)
     if export_type == 'restauration':
-        titles += ['mercredi_assis', 'mercredi_plateau', 'jeudi_assis', 'jeudi_plateau']
+        titles += ['mercredi', 'jeudi']
         yield writer.writerow(titles)
         for row in data:
             vals = []
             vals.append(row.get('id', ''))
-            vals.append(row['sections']['restauration']['dishes']['mercredi'].get('Assis', 0))
-            vals.append(row['sections']['restauration']['dishes']['mercredi'].get('Plateau-Repas', 0))
-            vals.append(row['sections']['restauration']['dishes']['jeudi'].get('Assis', 0))
-            vals.append(row['sections']['restauration']['dishes']['jeudi'].get('Plateau-Repas', 0))
+            vals.append(row['sections']['catering']['wed'].get('seated', 0))
+            vals.append(row['sections']['catering']['thu'].get('seated', 0))
             vals = [csv_encode(v) for v in vals]
             yield writer.writerow(vals)
     if export_type == 'transport':
         titles += ['departure_place', 'arrival_place', 'nb_persons', 'comment', 'phone', 'departure_time']
         yield writer.writerow(titles)
         for row in data:
-            for t in row['sections']['transport']['transports']:
+            for t in row['sections']['transports']:
                 vals = []
                 vals.append(row.get('id', ''))
                 for title in titles[1:]:
@@ -51,7 +49,7 @@ def generate_vals(writer, export_type, data):
         titles += ['name', 'function', 'days']
         yield writer.writerow(titles)
         for row in data:
-            for t in row['sections']['badges']['persons']:
+            for t in row['sections']['persons']:
                 vals = []
                 vals.append(row.get('id', ''))
                 for title in titles[1:]:
