d93c5cf03d9212263ff7753edadeac3eebd0eeed
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-02-12 02:14:38 +0100

    Deleting useless bits

---
+++ forum/assets.py
@@ -0,0 +1,38 @@
+import os
+from flask_assets import Bundle, Environment
+from forum import app
+
+assets = Environment(app)
+
+assets.append_path(os.path.join(os.path.dirname(__file__), './static'))
+assets.append_path(os.path.join(os.path.dirname(__file__), './static/bower_components'))
+
+bundles = {
+    'js_index': Bundle(
+        'recaptcha/index.js',
+        'bootstrap/dist/js/bootstrap.min.js',
+        'jquery/dist/jquery.min.js',
+        'typed.js/dist/typed.min.js',
+        'jquery.scrollTo/jquery.scrollTo.min.js',
+        'jQuery-One-Page-Nav/jquery.nav.js',
+        'js/index/index.js',
+        'iCheck/icheck.min.js',
+        'js/index/login.js',
+        output='gen/index.min.js',
+        filters='jsmin'),
+
+    'css_index': Bundle(
+        'bootstrap/dist/css/bootstrap.min.css',
+        'css/index/colors/blue.css',
+        'css/index/typography/typography-1.css',
+        'css/index/nemo.css',
+        'css/index/index.css',
+        'iCheck/skins/square/blue.css',
+        'css/index/login.css',
+        output='gen/index.min.css',
+        filters='cssmin'),
+
+    # Add other bundles
+}
+
+assets.register(bundles)
