1f31cbcd0eb7c455937e593a54650e7eaa4f286e
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-02-12 04:49:02 +0100

    Make login and index work

--- forum/assets.py
+++ forum/assets.py
@@ -8,35 +8,55 @@ assets.append_path(os.path.join(os.path.dirname(__file__), './static'))
 assets.append_path(os.path.join(os.path.dirname(__file__), './static/bower_components'))
 
 bundles = {
+    'js_common': Bundle(
+        Bundle(
+            'jquery/dist/jquery.min.js',
+        ),
+        output='build/common.min.js'),
+
     'js_index': Bundle(
         Bundle(
             'recaptcha/index.js',
-            'jquery/dist/jquery.min.js',
             'typed.js/dist/typed.min.js',
             'jquery.scrollTo/jquery.scrollTo.min.js',
             'jQuery-One-Page-Nav/jquery.nav.js',
-            'iCheck/icheck.min.js',
         ),
         Bundle(
             'js/index/index.js',
-            'js/index/login.js',
             filters='jsmin',
         ),
         output='build/index.min.js'),
 
-    'css_index': Bundle(
+    'js_login': Bundle(
+        Bundle(
+            'js/index/login.js',
+            filters='jsmin',
+        ),
+        output='build/login.min.js'),
+
+    'css_common': Bundle(
         Bundle(
             'bootstrap/dist/css/bootstrap.min.css',
+            'font-awesome/css/font-awesome.min.css',
+            filters='cssrewrite'
         ),
+        output='build/common.min.css'),
+
+    'css_index': Bundle(
         Bundle(
-            'css/index/colors/blue.css',
-            'iCheck/skins/square/blue.css',
             'css/index/nemo.css',
+            'css/index/colors/blue.css',
             filters='cssmin'
         ),
         output='build/index.min.css'),
 
-    # Add other bundles
+    'css_login': Bundle(
+        Bundle(
+            'css/index/login.css',
+            filters='cssmin'
+        ),
+        output='build/login.min.css'),
+
 }
 
 assets.register(bundles)
