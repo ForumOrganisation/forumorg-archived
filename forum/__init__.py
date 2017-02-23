6334501b0433ca4e8b3eb6f4b49784ba71c31af1
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-02-23 10:26:59 +0100

    Add SSL

--- forum/__init__.py
+++ forum/__init__.py
@@ -9,6 +9,7 @@ from flask_admin import Admin
 from flask_admin.base import MenuLink
 from flask_login import LoginManager
 from gridfs import GridFS
+from flask_sslify import SSLify
 from flask_babelex import Babel
 from flask_cdn import CDN
 
@@ -40,6 +41,9 @@ babel = Babel(app)
 # CDN
 cdn = CDN()
 cdn.init_app(app)
+
+# SSLify
+sslify = SSLify(app)
 
 
 @babel.localeselector
