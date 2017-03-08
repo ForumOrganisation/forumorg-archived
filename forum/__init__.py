dda5d4eefb2e349f160e7d0065e426bbc9099ace
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-03-08 05:06:21 +0100

    Fix cvtheque

--- forum/__init__.py
+++ forum/__init__.py
@@ -91,8 +91,7 @@ def get_resumes():
         for u in users:
             u['name'] = u'{} {}'.format(u.pop('name', None), u.pop('first_name', None))
             u['resume_url'] = url_for('get_resume', oid=u.pop('resume_id', None))
-        print(users)
-        return users
+        return users[:100]
     return dict(get_resumes=_get_resumes)
 
 
