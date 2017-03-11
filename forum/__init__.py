9f2ec968449aaf83c13aa82205b59d328b8f39d1
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-03-11 18:42:19 +0100

    Force ssl

--- forum/__init__.py
+++ forum/__init__.py
@@ -11,6 +11,7 @@ from flask_admin import Admin
 from flask_admin.base import MenuLink
 from flask_login import LoginManager
 from gridfs import GridFS
+from flask_sslify import SSLify
 from flask_babelex import Babel
 from flask_cdn import CDN
 
@@ -43,6 +44,9 @@ babel = Babel(app)
 # CDN
 cdn = CDN()
 cdn.init_app(app)
+
+# SSLify
+sslify = SSLify(app)
 
 
 @babel.localeselector
